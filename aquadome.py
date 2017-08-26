a74141da72074d314545cd6e2d511a8e10007118
Bobby Wade <bwade1@nc.rr.com>
2017-08-26 13:31:10 -0400

    Adding proxies.

--- aquadome.py
+++ aquadome.py
@@ -1,18 +1,40 @@
 #!/usr/local/bin/python3
 
+import threading
+import time
 
 from growbed import Growbed
 from fishtank import Fishtank
 from sumptank import Sumptank
 
 
-class Aquadome:
+class Aquadome(threading.Thread):
     def __init__(self):
+        threading.Thread.__init__(self)
+        self.threadID = 1
+        self.name = "Aquadome"
+        self.exitflag = 0
         self.fishtank = Fishtank()
         self.growbeds = {Growbed("GB-1"), Growbed("GB-2")}
         self.sumptank = Sumptank()
         self.airtemp = 0
         self.ventstatus = "closed"
+
+    def run(self):
+        print("Starting " + self.name)
+        while (True):
+            print('ok')
+            for gb in self.growbeds:
+                print(gb.getmydesignation())
+            self.fishtank.feedfish()
+            self.fishtank.setph(6.8)
+            print("ph: %0.2f" % self.fishtank.getph())
+            self.fishtank.setwatertemp(75)
+            print("water temp: %0.1f" % self.fishtank.getwatertemp())
+            self.setairtemp(75)
+            print("air temp: %0.1f" % self.getairtemp())
+            time.sleep(2)
+        print("Exiting " + self.name)
 
     def getairtemp(self):
         return self.airtemp
@@ -28,13 +50,4 @@ class Aquadome:
         self.ventstatus = "closed"
 
     def main(self):
-        print('ok')
-        for gb in self.growbeds:
-            print(gb.getmydesignation())
-        self.fishtank.feedfish()
-        self.fishtank.setph(6.8)
-        print("ph: %0.2f" % self.fishtank.getph())
-        self.fishtank.setwatertemp(75)
-        print("water temp: %0.1f" % self.fishtank.getwatertemp())
-        self.setairtemp(75)
-        print("air temp: %0.1f" % self.getairtemp())
+        self.run()
